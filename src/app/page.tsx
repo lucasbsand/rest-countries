564abaf1dd976e0b044a48a93759e21a3514d4db
lucasbsand <124009743+lucasbsand@users.noreply.github.com>
2024-07-14 17:38:33 -0300

    no results update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home({ searchParams }: SearchParamsProps) {
         <Search />
         <Filter />
       </div>
-      {countries ? (
+      {countries && countries.data.length > 0 ? (
         <section className="grid grid-cols-1 gap-12 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {countries.data.map((country) => (
             <Card
